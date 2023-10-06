ac244a9352141d538de5959ca45656a3acf2292f
hoapng <giahoa01giathieu@gmail.com>
2023-10-06 21:41:15 +0700

    create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = this.getHashPassword(createUserDto.password);
+    let user = await this.userModel.create(createUserDto);
+    return user;
   }
 
   findAll() {
