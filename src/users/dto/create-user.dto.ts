c962e1452537fbd79c2fb9a74e3cfcde8bef7953
hoapng <giahoa01giathieu@gmail.com>
2023-10-07 10:36:35 +0700

    validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
   name: string;
+  address: string;
+  phone: string;
+  age: number;
 }
