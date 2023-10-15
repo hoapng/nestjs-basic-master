127029c4802d8ac9831449d6c2514c5401e4e747
hoapng <giahoa01giathieu@gmail.com>
2023-10-15 19:02:29 +0700

    CRUD jobs

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,34 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { ResponseMessage, UserDecor } from 'src/decorator/customize';
+import { IUser } from 'src/users/user.interface';
 
 @Controller('jobs')
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Post()
-  create(@Body() createJobDto: CreateJobDto) {
-    return this.jobsService.create(createJobDto);
+  @ResponseMessage('Create job successfully')
+  create(@Body() createJobDto: CreateJobDto, @UserDecor() user: IUser) {
+    return this.jobsService.create(createJobDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  @ResponseMessage('Get all jobs successfully with pagination')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() qs: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, qs);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
-    return this.jobsService.update(+id, updateJobDto);
+  @ResponseMessage('Update job successfully')
+  update(
+    @Param('id') id: string,
+    @Body() updateJobDto: UpdateJobDto,
+    @UserDecor() user: IUser,
+  ) {
+    return this.jobsService.update(id, updateJobDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
+  remove(@Param('id') id: string, @UserDecor() user: IUser) {
+    return this.jobsService.remove(id, user);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -33,6 +33,7 @@ export class UsersController {
   ) {
     return this.usersService.findAll(+currentPage, +limit, qs);
   }
+
   @Public()
   @Get(':id')
   @ResponseMessage('Get user successfully')
